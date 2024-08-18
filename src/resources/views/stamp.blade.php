a74584660cd67cb3c3de5e866d28eb67a36961a5
kenichi tnk <ken.1978.tanaka@gmail.com>
2024-08-18 09:16:55 +0900

    css修正

--- src/resources/views/stamp.blade.php
+++ src/resources/views/stamp.blade.php
@@ -5,24 +5,25 @@
 @endsection
 
 @section('content')
-<div class="stamp__alert">
-    //メッセージ
-</div>
-
 <div class="stamp__content">
-    <div class="stamp__panel">
-        <form class="stamp__button">
-            <button class="stamp__button-submit" type="submit">勤務開始</button>
-        </form>
-        <form class="stamp__button">
-            <button class="stamp__button-submit" type="submit">勤務終了</button>
-        </form>
-        <form class="stamp__button">
-            <button class="stamp__button-submit" type="submit">休憩開始</button>
-        </form>
-        <form class="stamp__button">
-            <button class="stamp__button-submit" type="submit">休憩終了</button>
-        </form>
+    <div class="stamp__alert">
+        <h2><?php $user = Auth::user(); ?>{{ $user->name }}さんお疲れ様です！</h2>
     </div>
+        <div class="stamp__work-panel">
+            <form class="stamp__button">
+                <button class="stamp__button-submit" type="submit">勤務開始</button>
+            </form>
+            <form class="stamp__button">
+                <button class="stamp__button-submit" type="submit">勤務終了</button>
+            </form>
+        </div>
+        <div class="stamp__break-panel">
+            <form class="stamp__button">
+                <button class="stamp__button-submit" type="submit">休憩開始</button>
+            </form>
+            <form class="stamp__button">
+                <button class="stamp__button-submit" type="submit">休憩終了</button>
+            </form>
+        </div>
 </div>
 @endsection
